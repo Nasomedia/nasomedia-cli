5ab03807da5b99187ceca4e520ee009e12ec8fdf
hwc9169 <hwc9169@gmail.com>
2021-11-11 10:10:06 +0900

    오타 수정

--- resource/app/models/model_frame.py
+++ resource/app/models/model_frame.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Datetime
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 
 from app.db import Base
